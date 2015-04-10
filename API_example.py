7e3ffd8a4e33ac344f7c69817a8494cbfdfb1f69
PaulSec <aos.paul@gmail.com>
2015-04-10 15:27:01 +0100

    added the API example usage

---
+++ API_example.py
@@ -0,0 +1,16 @@
+from MalwrAPI import MalwrAPI
+
+res = MalwrAPI({'verbose': True}).get_recent_domains()
+print res 
+
+res = MalwrAPI({'verbose': True}).get_public_tags()
+print res 
+
+res = MalwrAPI({'verbose': True}).get_recent_analyses()
+print res 
+
+res = MalwrAPI({'verbose': True}).get_latest_comments()
+print res 
+
+res = MalwrAPI({'verbose': True}).submit_sample('/tmp/test.txt')
+print res
